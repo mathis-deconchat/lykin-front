38bb6cf502bbcc1447a354400c543ae3015476f2
mathis.deconchat@gmail.com <mathis.deconchat@webinage.fr>
2023-03-18 16:24:41 +0100

    Merge operation in develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import {
-  useCreateSpendingMutation,
-  useGetAllSpendingsQuery,
-} from "./generated/graphql-types";
+
 import AppRouter from "./services/router";
 
 function App() {
   return (
     <>
-      <AppRouter />
       <div className="bg-gradient-to-t from-blue-200 via-gray-600 to-gray-900 min-h-screen min-w-screen ">
-        <AppRouter />
+      <AppRouter />
       </div>
     </>
   );
