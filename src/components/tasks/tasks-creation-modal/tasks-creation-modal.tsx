18aad28e3d6a91ec446ada42310c8d048a5beb77
mathis.deconchat@gmail.com <mathis.deconchat@webinage.fr>
2023-03-23 11:23:53 +0100

    Task creation

--- src/components/tasks/tasks-creation-modal/tasks-creation-modal.tsx
+++ src/components/tasks/tasks-creation-modal/tasks-creation-modal.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Modal from "react-modal";
+import Divider from "../../common/divider";
 import "./tasks-creation-modal.css";
+import { ClockIcon } from "@heroicons/react/24/outline";
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import { useForm } from "react-hook-form";
+import {
+  CreateTaskInput,
+  useCreateTaskMutation,
+} from "../../../generated/graphql-types";
+import { ApolloClient } from "@apollo/client";
+import apolloClient from "../../../shared/apollo/apollo.provider";
 
 type TaskCreationModalProps = {
   modalIsOpen: boolean;
@@ -9,6 +19,45 @@ type TaskCreationModalProps = {
 };
 
 const TasksCreationModal: React.FC<TaskCreationModalProps> = (props) => {
+  const [createTaskMutation, { data, loading, error }] =
+    useCreateTaskMutation();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  const onSubmit =  (data: any) => {
+    console.log('Submit')
+    console.log(data)
+    const { name, description } = data;
+
+    const taskCreateInput: CreateTaskInput = {
+      task: {
+        name,
+        description,
+        statusId: 1,
+      },
+    };
+
+    const i = createTaskMutation({
+      variables: {
+        input: taskCreateInput,
+      }, onCompleted: (data) => {
+        console.log(data)
+      }, onError: (error) => {
+        console.log(error)
+      }
+    });
+    console.log(i)
+    props.closeModal();
+    apolloClient.refetchQueries({include: ['GetAllTasks']})
+  };
   return (
     <Modal
       isOpen={props.modalIsOpen}
@@ -22,24 +71,51 @@ const TasksCreationModal: React.FC<TaskCreationModalProps> = (props) => {
       }}
       contentLabel="OperationDetailsModal"
       className={
-        " rounded-xl absolute bottom-0 w-full  flex flex-col p-3 border-none focus:ring-0 focus:outline-none focus:border-0 hover:ring-0 hover:focus-0 active:border-0 active:ring-0"
+        "absolute bottom-0 rounded-xl  w-full  flex flex-col p-3 border-none focus:ring-0 focus:outline-none focus:border-0 hover:ring-0 hover:focus-0 active:border-0 active:ring-0"
       }
     >
-      <div className="bg-gradient-to-l from-blue-400 via-blue-500 to-blue-600 rounded-xl w-full p-4">
-        <div>
-          <input
-            autoFocus
-            type="text"
-            className=" placeholder-gray-100 outline-none border-transparent focus:border-0 focus:outline-offset-0 focus:outline-none visited:border-0 focus:border-none active:outline-none active:border-0 hover:outline-none    hover:border-0 bg-transparent py-2 border-none hover:outline-offset-0 border-0  text-gray-200 rounded-lg w-3/4 p-1 caret-white"
-            placeholder="Nom de la tâche"
-          ></input>
-          <input
-            type="text"
-            className="mt-3 placeholder-gray-100 bg-transparent border-transparent focus:outline-none focus:ring-0 w-full caret-white"
-            placeholder="Description"
-          ></input>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="bg-gradient-to-l from-blue-400 via-blue-500 to-blue-600 rounded-xl w-full p-4">
+          <div>
+            <input
+              {...register("name", { required: true })}
+              autoFocus
+              className="py-3 text-lg placeholder-gray-100 placeholder:font-bold outline-transparent placeholder:opacity-50  text-gray-200 rounded-lg w-3/4 p-1 caret-white bg-transparent"
+              placeholder="Nom de la tâche"
+            ></input>
+
+            <input
+              {...register("description", { required: true })}
+              type="textarea"
+              className="mt-1 p-1 bg-blue-600 placeholder-gray-100  outline-transparent placeholder:opacity-50  bg-transparent border-transparent focus:outline-none focus:ring-0 w-full caret-white"
+              placeholder="Description"
+            ></input>
+            <div className="flex space-x-2 overflow-x-auto mt-4 w-full pb-3">
+              <div className=" bg-blue-800 py-1 border-spacing-4 w-fit text-white text-opacity-90 px-2  rounded-xl flex items-center space-x-2">
+                <ClockIcon className="h-5 w-5 stroke-2" />
+                <p className=" whitespace-nowrap">Aujourd'hui</p>
+              </div>
+              <div className=" border-gray-300 border-[1px] py-1 border-spacing-4 w-fit text-white text-opacity-90 px-2  rounded-xl flex items-center space-x-2">
+                <ClockIcon className="h-5 w-5 stroke-2" />
+                <p className=" whitespace-nowrap">Demain</p>
+              </div>
+              <div className=" border-gray-300 border-[1px] py-1 border-spacing-4 w-fit text-white text-opacity-90 px-2  rounded-xl flex items-center space-x-2">
+                <ClockIcon className="h-5 w-5 stroke-2" />
+                <p className=" whitespace-nowrap">Dans la semaine</p>
+              </div>
+              <div className=" border-gray-300 border-[1px] py-1 border-spacing-4 w-fit text-white text-opacity-90 px-2  rounded-xl flex items-center space-x-2">
+                <ClockIcon className="h-5 w-5 stroke-2" />
+                <p className=" whitespace-nowrap">Dans le mois</p>
+              </div>
+            </div>
+            <div className="w-full flex justify-end mt-3">
+              <button type="submit" className="rounded-xl bg-blue-800 p-2">
+                <PaperAirplaneIcon className="h-5 w-5 stroke-2 text-white" />
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
+      </form>
     </Modal>
   );
 };
--- src/pages/tasks/tasks-page.tsx
+++ src/pages/tasks/tasks-page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import TasksCreationModal from "../../components/tasks/tasks-creation-modal/tasks-creation-modal";
+import { useGetAllTasksQuery } from "../../generated/graphql-types";
 
 const TasksPage = () => {
+  const {data, loading, error} = useGetAllTasksQuery({fetchPolicy: 'network-only'})
   const [modalIsOpenTaskCreation, setModalIsOpenTaskCreation] =
     React.useState(false);
   function openTaskCreationModal() {
@@ -19,6 +21,24 @@ const TasksPage = () => {
         closeModal={closeTaskCreationModal}
         afterOpenModal={openTaskCreationModal}
       />
+      <div className="h-1/2 w-full">
+        {
+          (data && data.tasks && !loading) && data?.tasks.nodes.map((task: any) => {
+            return (
+              <div className="w-full h-16 bg-white rounded-lg shadow-lg flex flex-row justify-between items-center p-4">
+                <div className="flex flex-col">
+                  <p className="font-bold">{task.name}</p>
+                  <p className="text-gray-600">{task.description}</p>
+                </div>
+                <div className="flex flex-row">
+                  <p className="text-gray-600">{task.status.name}</p>
+                </div>
+              </div>
+            )
+          })
+        }
+
+      </div>
         <button className=" absolute bottom-24 right-2 px-5 bg-blue-600 rounded-lg py-1 text-white w-1/2" onClick={() => openTaskCreationModal()}>Créer</button>
       <div className="rounded-t-2xl bg-gray-100 w-full flex flex-col mb-14  p-4">
         <p className="font-bold text-xl">A faire</p>
