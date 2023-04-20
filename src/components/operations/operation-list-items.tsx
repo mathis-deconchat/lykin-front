b5f44868204e1419782eb3a2816c9c3dde25401a
mathis.deconchat@gmail.com <mathis.deconchat@webinage.fr>
2023-04-20 08:50:49 +0200

    Fixed integration front page

--- src/components/operations/operation-list-items.tsx
+++ src/components/operations/operation-list-items.tsx
@@ -17,19 +17,35 @@ type OperationListItemsProps = {
   openModal: () => void;
 };
 
+function convertDateToYearMonth(dateString: string) {
+  // Create a new Date object from the input string
+  const date = new Date(dateString);
+  
+  // Extract the year and month from the date object
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Add leading zero if needed
+  
+  // Return the year and month as a string in the format "yyyy-mm"
+  return `${year}-${month}`;
+}
+
+const toCurrencyFormat = (amount: string) => {
+  return (+amount).toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' });
+};
+
 const OperationListItems: React.FC<OperationListItemsProps> = (props) => {
   return (
-    <div className='flex mt-1 justify-between' onClick={() => props.openModal()}>
+    <div className='flex mt-1' onClick={() => props.openModal()}>
         <div className='rounded-full h-12 w-12 bg-gray-200 flex items-center justify-center align-middle'>
             <p className='text-lg font-bold'>M</p>
 
         </div>
         <div className='ml-3'>
             <p className='text-lg font-bold'>Mathis Deconchat</p>
-            <p className='text-sm text-gray-600'>{props.operation?.createdAt + ' ' + props.operation!.category!.name!}</p>
+            <p className='text-sm text-gray-600'>{convertDateToYearMonth(props.operation?.createdAt) + '- ' + props.operation!.category!.name!}</p>
         </div>
-        <div className='ml-3'>
-            <p className='mt-3  font-bold text-red-400 inline-block'>- {props.operation.amount}</p>
+        <div className='ml-auto inline-flex'>
+            <p className='mt-3  font-bold text-red-400 inline-block'>- {toCurrencyFormat(props.operation!.amount!)}</p>
 
         </div>
 
--- src/reducers/signup-reducer.ts
+++ src/reducers/signup-reducer.ts
@@ -13,3 +13,8 @@ const signupReducer: Reducer<SignUpState, SignUpAction> = (state, action): SignU
 };
 
 export default signupReducer;
+
+// Convert this date 2023-04-18T19:08:50.840777 Restauration to yyyy-mm 
+const date = "2023-04-18T19:08:50.840777";
+const date2 = date.split("T")[0];
+const date3 = date2.split("-")[0] + "-" + date2.split("-")[1];
